22c0ac75fabbf5547743cb54267981aee47d4bbd
Андрей <andrew121205@gmail.com>
2021-03-29 22:09:30 +0500

    yes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 def main():
     vk_session = vk_api.VkApi(
         token='64f7d38df6cbac49f2146d5037a93647b83f9897e355478551f3bee2d393cc2a8f57aefd5803bf5b88750')
-    longpoll = VkBotLongPoll(vk_session, '203632426')
+    longpoll = VkBotLongPoll(vk_session, 203632426)
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             vk = vk_session.get_api()
