4b03f7dfa69090c90614b746efb1e9473ee7c239
Андрей <andrew121205@gmail.com>
2021-04-18 13:26:29 +0500

    Подсчёт (калькулятор)

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import wikipedia
@@ -16,27 +15,38 @@ def main():
         if event.type == VkBotEventType.MESSAGE_NEW:
             vk = vk_session.get_api()
             txt_msg = event.obj.message['text']
-            print('Текст:', txt_msg)
-            print(event.obj.message['from_id'])
             url += txt_msg
-            if 'Прив' in txt_msg:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message="Здравствуйте",
-                                 random_id=random.randint(0, 2 ** 64))
-            else:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message="Попробуем найти...",
-                                 random_id=random.randint(0, 2 ** 64))
-                try:
-                    data = wikipedia.page(txt_msg).summary
-                    a = data.split('\n')
+            if '!' == txt_msg[0]:
+                if 'найди слово' == txt_msg[1:12].lower():
                     vk.messages.send(user_id=event.obj.message['from_id'],
-                                     message=f'{a[0]}',
+                                     message="Попробуем найти...",
                                      random_id=random.randint(0, 2 ** 64))
-                except:
+                    try:
+                        data = wikipedia.page(txt_msg[13:]).summary
+                        a = data.split('\n')
+                        vk.messages.send(user_id=event.obj.message['from_id'],
+                                         message=f'{a[0]}',
+                                         random_id=random.randint(0, 2 ** 64))
+                    except:
+                        vk.messages.send(user_id=event.obj.message['from_id'],
+                                         message='К сожалению произошла непредвиденная ошибка,'
+                                                 ' моя команда работает над её исправлением',
+                                         random_id=random.randint(0, 2 ** 64))
+                elif '!help' == txt_msg.lower():
                     vk.messages.send(user_id=event.obj.message['from_id'],
-                                     message='К сожалению произошла непредвиденная ошибка,'
-                                             ' моя команда работает над её исправлением',
+                                     message='!help - для показа всех команд'
+                                             '!найди слово <слово> - выводит определение слова из википедии',
+                                     random_id=random.randint(0, 2 ** 64))
+                elif 'посчитай' in txt_msg.lower() or 'калькулятор' in txt_msg.lower():
+                    calc = txt_msg.split()
+                    f = eval(calc[1])
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=f'{f}',
+                                     random_id=random.randint(0, 2 ** 64))
+                else:
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message='Команда введена неправильно'
+                                             'Для списка команд напишите !help',
                                      random_id=random.randint(0, 2 ** 64))
 
 
